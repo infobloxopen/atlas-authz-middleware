2dae9e5ebaf16d2277ae520f35543853f53647aa
AnPiakhota-Infoblox <apiakhota@infoblox.com>
2022-05-12 13:05:57 +0300

    Update opa to v0.40.0; fix test

--- pkg/eval/goapi/authorizer_test.go
+++ pkg/eval/goapi/authorizer_test.go
@@ -1,7 +1,6 @@
 package goapi
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -20,68 +19,9 @@ const (
 	reset   = "\033[0m"
 )
 
-func Test_Example(t *testing.T) {
-	ctx := context.Background()
-
-	// create a mock HTTP bundle server
-	server, err := sdktest.NewServer(sdktest.MockBundle("/bundles/bundle.tar.gz", map[string]string{
-		"example.rego": `
-			package authz
-
-			default allow := false
-
-			allow {
-				input.open == "sesame"
-			}
-		`,
-	}))
-	if err != nil {
-		// handle error.
-	}
-
-	defer server.Stop()
-
-	// provide the OPA configuration which specifies
-	// fetching policy bundles from the mock server
-	// and logging decisions locally to the console
-	config := []byte(fmt.Sprintf(`{
-		"services": {
-			"test": {
-				"url": %q
-			}
-		},
-		"bundles": {
-			"test": {
-				"resource": "/bundles/bundle.tar.gz"
-			}
-		},
-		"decision_logs": {
-			"console": true
-		}
-	}`, server.URL()))
-
-	// create an instance of the OPA object
-	opa, err := sdk.New(ctx, sdk.Options{
-		Config: bytes.NewReader(config),
-	})
-
-	if err != nil {
-		// handle error.
-	}
-
-	defer opa.Stop(ctx)
-
-	// get the named policy decision for the specified input
-	if result, err := opa.Decision(ctx, sdk.DecisionOptions{Path: "/authz/allow", Input: map[string]interface{}{"open": "sesame"}}); err != nil {
-		// handle error.
-	} else if decision, ok := result.Result.(bool); !ok || !decision {
-		// handle error.
-	}
-}
-
 func Test_autorizer_Authorize(t *testing.T) {
 	// create a mock HTTP bundle server
-	server, err := sdktest.NewServer(sdktest.MockBundle("/bundles/bundle.tar.gz", map[string]string{
+	server := sdktest.MustNewServer(sdktest.MockBundle("/bundles/bundle.tar.gz", map[string]string{
 		"example.rego": `
 				package authz
 
@@ -92,9 +32,6 @@ func Test_autorizer_Authorize(t *testing.T) {
 				}
 			`,
 	}))
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	defer server.Stop()
 
@@ -122,7 +59,9 @@ func Test_autorizer_Authorize(t *testing.T) {
 			input: map[string]interface{}{
 				"open": "sesame",
 			},
-			wantRes: &sdk.DecisionResult{},
+			wantRes: &sdk.DecisionResult{
+				Result: true,
+			},
 			wantErr: false,
 		},
 	}
@@ -130,15 +69,25 @@ func Test_autorizer_Authorize(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			res, err := a.Authorize(tt.ctx, tt.input)
 			// check error
-			if err != nil && tt.wantErr {
-				t.Errorf("\t%s unexpected error when running %s test"+
-					"\nGot: %s\nWant error: %t", failed, tt.name, err.Error(), tt.wantErr)
-				return
-			} else {
-				t.Logf("\t%s %s test is passed", succeed, tt.name)
-				return
+			if err != nil {
+				if !tt.wantErr {
+					t.Errorf("\t%s unexpected error when running %s test"+
+						"\nGot: %s\nWant error: %t", failed, tt.name, err.Error(), tt.wantErr)
+					return
+				} else {
+					t.Logf("\t%s %s test is passed", succeed, tt.name)
+					return
+				}
 			}
+
 			// check result
+			t.Logf("Decision result: %+v", res)
+			// remove ID as it gets regenerated on every run
+			if r, ok := res.(*sdk.DecisionResult); ok {
+				r.ID = ""
+				res = r
+			}
+
 			if !reflect.DeepEqual(res, tt.wantRes) {
 				resJSON, err := json.MarshalIndent(res, "", "    ")
 				if err != nil {
